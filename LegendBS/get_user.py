4e3184d06b3df4020573d893a8c50e5f18af191b
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-23 07:07:07 +0530

    Update get_user.py

--- LegendBS/get_user.py
+++ LegendBS/get_user.py
@@ -28,14 +28,3 @@ async def get_user(Legend, message):
         return
 
     return user
-
-
-def user_errors(error):
-    if '[400 USERNAME_NOT_OCCUPIED]' in str(error):
-       return "You didn't provide username"
-    elif '[400 USERNAME_INVALID]' in str(error):
-       return "Username is invalid"
-    elif '[400 PEER_ID_INVALID]' in str(error):
-       return "Invalid User ID!"
-    else:
-       return f"**Unknown Error:** \n\n {error}"
