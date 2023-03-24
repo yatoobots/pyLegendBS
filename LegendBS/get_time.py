dbd79d4482086705d0f97d4e1b847231846a53e2
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-24 12:42:11 +0530

    Create get_time.py

---
+++ LegendBS/get_time.py
@@ -0,0 +1,11 @@
+def get_time(time: int):
+    if time >= 86400:
+        time = int(time / (60 * 60 * 24))
+        unit = "days"
+    elif time >= 3600 < 86400:
+        time = int(time / (60 * 60))
+        unit = "hours"
+    elif time >= 60 < 3600:
+        time = int(time / 60)
+        unit = "minutes"
+    return "{} {}".format(time, unit)
