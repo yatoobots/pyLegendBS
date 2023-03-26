48523e3c7558371fd5be62dec69bdc0254ad4904
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-26 14:23:48 +0530

    Update get_time.py

--- LegendBS/get_time.py
+++ LegendBS/get_time.py
@@ -2,10 +2,13 @@ def get_time(time: int):
     if time >= 86400:
         time = int(time / (60 * 60 * 24))
         unit = "days"
+    ftime = f"{time} {unit}"
     elif time >= 3600 < 86400:
         time = int(time / (60 * 60))
         unit = "hours"
+    ftime = f"{time} {unit}"
     elif time >= 60 < 3600:
         time = int(time / 60)
         unit = "minutes"
-    return " {} {}".format(time, unit)
+    ftime = f"{time} {unit}"
+    return ftime
