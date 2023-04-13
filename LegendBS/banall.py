9ea2f9fa456331dd0f4a68fdab68ec1d2007249d
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-04-13 14:53:00 +0530

    Update banall.py

--- LegendBS/banall.py
+++ LegendBS/banall.py
@@ -1,19 +1,19 @@
 async def start_banall(Legend, message):
-   chat = message.chat
-   a = await client.get_chat_member(chat.id, 'me')
-   if a.status = "administrator":
-       x = await Legend.send_message(chat.id, "Hey it's Legend Bot Spam")
-       done = 0
-       failed = 0
-       async for u in Legend.get_chat_members(chat.id):
-          user = u.user
-          try:
-             await Legend.ban_chat_member(chat.id, user.id)
-             done += 1
-          except Exception as err:
+    chat = message.chat
+    a = await client.get_chat_member(chat.id, 'me')
+    if a.status = "administrator":
+        x = await Legend.send_message(chat.id, "Hey it's Legend Bot Spam")
+        done = 0
+        failed = 0
+        async for u in Legend.get_chat_members(chat.id):
+            user = u.user
+            try:
+                await Legend.ban_chat_member(chat.id, user.id)
+                done += 1
+            except Exception as err:
              print(f"Legend Bot Spam- [INFO]: {str(err)}")
-             failed += 1
-       await x.delete()
-       await Legend.send_message(chat.id, f"Members Banned ✓ \n\n Banned {done} users\n failed {failed}")
-   else:
-       await Legend.send_message(chat.id, "Promote me to admin😭")
+                failed += 1
+        await x.delete()
+        await Legend.send_message(chat.id, f"Members Banned ✓ \n\n Banned {done} users\n failed {failed}")
+    else:
+        await Legend.send_message(chat.id, "Promote me to admin😭")
