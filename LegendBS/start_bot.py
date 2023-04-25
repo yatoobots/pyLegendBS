177c4976cf4105fb8001c7af878c14c78db99e57
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-04-25 13:33:53 +0530

    Update start_bot.py

--- LegendBS/start_bot.py
+++ LegendBS/start_bot.py
@@ -1,3 +1,6 @@
+from pyrogram.types import BotCommand
+
+
 async def start_bot(Client):
     await Client.start()
     try:
@@ -5,3 +8,21 @@ async def start_bot(Client):
         print(f"Client - [INFO]: @{x.username} get started ")
     except Exception as e:
         print(f"Error - {e}")
+    try:
+        print("Settings All Commands")
+        await Client.set_bot_commands(
+            [
+                BotCommand("start", "Start Bot By Anyone"),
+                BotCommand("ping", "Check that bot is alive or dead"),
+                BotCommand("banall", "banall the member in Group"),
+                BotCommand("birthday", "spam the chat with birthday message"),
+                BotCommand("restart", "Restart The Bot"),
+                BotCommand("eval", "Run Python Code"),
+                BotCommand("exec", "Install The Requirements"),
+                BotCommand("gm", "Spam The Chat with good morning message"),
+                BotCommand("ga", "Spam The Chat With Good Afternoon Messages")
+            ]
+        )
+    except:
+        pass
+        
